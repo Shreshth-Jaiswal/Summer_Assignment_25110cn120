a71121724699787acacdb4e714211c438f10c294
Shreshth Jaiswal <jaiswalshreshth213@gmail.com>
2026-05-28 17:57:45 +0530

    Uploaded Q1

--- Day_1/Q1.c
+++ Day_1/Q1.c
@@ -1,9 +1,14 @@
+// Write a program to Calculate sum of first N natural number.
 #include <stdio.h>
 int main()
 {
-    int a,b;
-    a=5;
-    b=6;
-    printf("%d",a+b);
+    int n,sum=0;
+    printf("Enter the number to calculate the sum upto that number :");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        sum+=i+1;
+    }
+    printf("Sum of first N natural number = %d",sum);
     return 0;
 }
